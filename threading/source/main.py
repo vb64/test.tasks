f628d14033cb2ac97a4e1d631e64a62bfc35d25a
Vitaly Bogomolov <mail@vitaly-bogomolov.ru>
2021-02-01 23:14:55 +0400

    add xml parser

--- threading/source/main.py
+++ threading/source/main.py
@@ -3,6 +3,7 @@
 from threading import Thread
 from random import randint
 from zipfile import ZipFile
+from xml.sax import ContentHandler, parseString
 
 try:
     from queue import Queue
@@ -20,10 +21,10 @@ XML_LEVEL_MAX = 100
 XML_OBJECTS_MIN = 1
 XML_OBJECTS_MAX = 10
 
-XML_OBJECT_TEMPLATE = "<objectname='{}'/>"
+XML_OBJECT_TEMPLATE = "<object name='{}'/>"
 XML_TEMPLATE = """<root>
-<varname='id' value='{}'/>
-<varname='level' value='{}'/>
+<var name='id' value='{}'/>
+<var name='level' value='{}'/>
 <objects>
 {}
 </objects>
@@ -45,17 +46,44 @@ def make_xml(zip_id, xml_num):
     )
 
 
-def make_zip(input_queue, thread_id):
-    """Get zip file names from input_queue and write zip file with xml files inside."""
+class NgenixXml(ContentHandler):
+    """Ngenix xml sax parser."""
+
+    def __init__(self, csv1, csv2):
+        ContentHandler.__init__(self)
+        self.var_id = None
+        self.csv1 = csv1
+        self.csv2 = csv2
+
+    def startElement(self, name, attrs):
+        """Put parsed values to csv queues."""
+        if name == 'var':
+            attr_name = attrs.getValue('name')
+            if attr_name == 'id':
+                self.var_id = attrs.getValue('value')
+            elif attr_name == 'level':
+                self.csv1.put((self.var_id, attrs.getValue('value')))
+        elif name == 'object':
+            self.csv2.put((self.var_id, attrs.getValue('name')))
+
+
+def handle_zip(mode, input_queue, thread_id, csv1, csv2):
+    """Get zip file names from input_queue and handle according mode."""
     count = 0
     while True:
         task_id = input_queue.get()
         if task_id is None:
             break
 
-        with ZipFile("{}.zip".format(task_id), 'w') as zfile:
-            for i in range(XML_IN_ZIP):
-                zfile.writestr("{}.xml".format(i), make_xml(task_id, i))
+        with ZipFile("{}.zip".format(task_id), mode) as zfile:
+            if mode == 'w':
+                for i in range(XML_IN_ZIP):
+                    zfile.writestr("{}.xml".format(i), make_xml(task_id, i))
+            else:
+                parser = NgenixXml(csv1, csv2)
+                for i in range(XML_IN_ZIP):
+                    parser.var_id = None
+                    parseString(zfile.read("{}.xml".format(i)), parser)
 
         count += 1
         input_queue.task_done()
@@ -64,17 +92,21 @@ def make_zip(input_queue, thread_id):
     input_queue.task_done()
 
 
-def main():
-    """Write zipz, read zips/xml, write csv."""
-    queue_zip = Queue()
-    for i in range(ZIP_FILES):  # fill zip file names for writing
+def init_zip_queue(queue_zip):
+    """Fill zip file names and terminator marks for for working threads."""
+    for i in range(ZIP_FILES):
         queue_zip.put(i)
-
-    for i in range(ZIP_THREADS):  # fill terminator marks for working threads
+    for i in range(ZIP_THREADS):
         queue_zip.put(None)
-    for i in range(ZIP_THREADS):  # run zip writers
-        Thread(target=make_zip, args=(queue_zip, i)).start()
 
+
+def main():
+    """Write zips, read zips/xml, write csv."""
+    queue_zip = Queue()
+    init_zip_queue(queue_zip)
+
+    for i in range(ZIP_THREADS):
+        Thread(target=handle_zip, args=('w', queue_zip, i, None, None)).start()
     queue_zip.join()
 
     zips = 0
@@ -82,7 +114,20 @@ def main():
         print("{}: {}".format(i, LOG[i]))
         zips += LOG[i]
 
-    print("Total: {}".format(zips))
+    print("Zips created: {}".format(zips))
+
+    queue_csv1 = Queue()
+    queue_csv2 = Queue()
+    init_zip_queue(queue_zip)
+
+    LOG.clear()
+
+    for i in range(ZIP_THREADS):
+        Thread(target=handle_zip, args=('r', queue_zip, i, queue_csv1, queue_csv2)).start()
+    queue_zip.join()
+
+    print("Csv1: {}".format(queue_csv1.qsize()))
+    print("Csv2: {}".format(queue_csv2.qsize()))
 
 
 if __name__ == '__main__':
