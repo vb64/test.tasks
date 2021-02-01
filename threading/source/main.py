f32f31152210e51c16eb529c1f4364f37f6b5f9d
Vitaly Bogomolov <mail@vitaly-bogomolov.ru>
2021-02-01 16:28:12 +0400

    add comments

--- threading/source/main.py
+++ threading/source/main.py
@@ -1,17 +1,34 @@
+"""
+IO for zip/xml/csv files
+"""
+
 import threading
 from Queue import Queue
+from zipfile import ZipFile
+
+XML_TEMPLATE = """<root>
+</root>
+"""
 
 ZIP_FILES = 50
 ZIP_THREADS = 10
+XML_IN_ZIP = 100
 LOG = {}
 
 
 def make_zip(input_queue, thread_id):
+    """ Get zip file names from input_queue and write zip file with xml files inside."""
     count = 0
     while True:
         task_id = input_queue.get()
         if task_id is None:
             break
+
+        zfile = ZipFile("{}.zip".format(task_id), 'w')
+        for i in range(XML_IN_ZIP):
+            zfile.writestr("{}.xml".format(i), XML_TEMPLATE)
+        zfile.close()
+
         count += 1
         input_queue.task_done()
 
@@ -20,13 +37,14 @@ def make_zip(input_queue, thread_id):
 
 
 def main():
+    """Write zipz, read zips/xml, write csv."""
     queue_zip = Queue()
-    for i in range(ZIP_FILES):
+    for i in range(ZIP_FILES):  # fill zip file names for writing
         queue_zip.put(i)
-    for i in range(ZIP_THREADS):
+    
+    for i in range(ZIP_THREADS):  # fill terminator marks for working threads
         queue_zip.put(None)
-
-    for i in range(ZIP_THREADS):
+    for i in range(ZIP_THREADS):  # run zip writers
         threading.Thread(target=make_zip, args=(queue_zip, i)).start()
 
     queue_zip.join()
@@ -39,5 +57,5 @@ def main():
     print "Total:", zips
 
 
-if __name__ == '__main__':  # pragma: no cover
+if __name__ == '__main__':
     main()
