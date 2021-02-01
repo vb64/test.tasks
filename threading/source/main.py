931b160cc06c0ae2a9c3af60041e72b65f7dafaa
Vitaly Bogomolov <mail@vitaly-bogomolov.ru>
2021-02-01 17:00:55 +0400

    make_xml function

--- threading/source/main.py
+++ threading/source/main.py
@@ -1,23 +1,49 @@
 """
 IO for zip/xml/csv files
 """
-
 import threading
+import random
 from Queue import Queue
 from zipfile import ZipFile
 
-XML_TEMPLATE = """<root>
-</root>
-"""
+LOG = {}
 
 ZIP_FILES = 50
 ZIP_THREADS = 10
 XML_IN_ZIP = 100
-LOG = {}
+
+XML_LEVEL_MIN = 1
+XML_LEVEL_MAX = 100
+XML_OBJECTS_MIN = 1
+XML_OBJECTS_MAX = 10
+
+XML_OBJECT_TEMPLATE = "<objectname='{}'/>"
+XML_TEMPLATE = """<root>
+<varname='id' value='{}'/>
+<varname='level' value='{}'/>
+<objects>
+{}
+</objects>
+</root>
+"""
+
+
+def make_xml(zip_id, xml_num):
+    """Create xml content according rules."""
+    xml_id = "Z{}X{}".format(zip_id, xml_num)
+    obj_range = range(random.randint(XML_OBJECTS_MIN, XML_OBJECTS_MAX))
+
+    return XML_TEMPLATE.format(
+      xml_id,
+      random.randint(XML_LEVEL_MIN, XML_LEVEL_MAX),
+      '\n'.join([
+        XML_OBJECT_TEMPLATE.format("{}OBJ{}".format(xml_id, i)) for i in obj_range
+      ])
+    )
 
 
 def make_zip(input_queue, thread_id):
-    """ Get zip file names from input_queue and write zip file with xml files inside."""
+    """Get zip file names from input_queue and write zip file with xml files inside."""
     count = 0
     while True:
         task_id = input_queue.get()
@@ -26,7 +52,7 @@ def make_zip(input_queue, thread_id):
 
         with ZipFile("{}.zip".format(task_id), 'w') as zfile:
             for i in range(XML_IN_ZIP):
-                zfile.writestr("{}.xml".format(i), XML_TEMPLATE)
+                zfile.writestr("{}.xml".format(i), make_xml(task_id, i))
 
         count += 1
         input_queue.task_done()
